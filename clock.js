53f77d4d448535a4a8394f1b012fe52c66e8efd9
yohan oh <yohan.oh332@gmail.com>
2021-07-13 18:31:21 -0400

    completed clock.js

--- clock.js
+++ clock.js
@@ -5,8 +5,7 @@ class Clock {
     const time = new Date ();
 
     // 2. Store the hours, minutes, and seconds.
-
-    this.hours = time.getHours();
+    this.hours = 23;
     this.minutes = time.getMinutes();
     this.seconds = time.getSeconds();
 
@@ -14,7 +13,7 @@ class Clock {
     this.printTime();
 
     // 4. Schedule the tick at 1 second intervals.
-    setInterval(this._tick.bind(this), 1000)
+    setInterval(this._tick.bind(this), 10)
     }
 
     printTime() {
@@ -27,7 +26,23 @@ class Clock {
     _tick() {
     // 1. Increment the time by one second.
     
+    this.seconds += 1; 
+    if (this.seconds === 60) {
+        this.seconds = 0;
+        this.minutes += 1;
+
+        if (this.minutes === 60) {
+            this.minutes = 0;
+            this.hours += 1;
+
+            if (this.hours === 24) {
+                this.hours = 0;
+            }
+        }
+    }    
+
     // 2. Call printTime.   
+    this.printTime();
     }
 
 }
