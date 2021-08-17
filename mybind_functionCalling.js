97992869dcf60e3870f24df113d73832662469af
yohan oh <yohan.oh332@gmail.com>
2021-08-16 22:27:51 -0400

    completed mybind..js

--- mybind_functionCalling.js
+++ mybind_functionCalling.js
@@ -1,3 +1,16 @@
 Function.prototype.myBind = function(context) {
     return () => this.apply(context)
 }
+
+class Cat {
+    constructor(name) {
+        this.name = name;
+    }
+
+    meow() {
+        console.log(`${this.name} says "meow"`)
+    }
+}
+
+const spots = new Cat("Spots");
+setTimeout(spots.meow.myBind(spots), 1000);
